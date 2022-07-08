4e548e2bbbc40cbf7fc1d65f274ae573bb48004b
felipe.silva <felipe.silva@ipm.com.br>
2022-07-08 14:20:02 -0300

    feat: esture SignUpController return 400 if an invalid email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,14 +2,26 @@ import { MissingParamError } from '../errors/missing_param_error'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email_validator'
+import { InvalidParamError } from '../errors/invalid_param_error'
 
 export class SignupController implements Controller {
+  private readonly emailValidador: EmailValidator
+
+  public constructor (emailValidador: EmailValidator) {
+    this.emailValidador = emailValidador
+  }
+
   public handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const isValid = this.emailValidador.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
     return {
       statusCode: 200,
