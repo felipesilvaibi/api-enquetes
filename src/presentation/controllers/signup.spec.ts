521e125ac63b7da574696c82c0b94d150504be60
Felipe da Silva <felipe_ibirama@hotmail.com>
2022-07-08 01:24:42 -0300

    test: add basic signup test

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignupController } from './signup'
+
+describe('SignupController', () => {
+  test('Caso o nome não for fornecido será retornado um erro 400', () => {
+    const sut = new SignupController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
