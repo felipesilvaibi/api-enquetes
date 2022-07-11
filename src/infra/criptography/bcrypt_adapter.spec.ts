c108164cd20828cb93cf5fe3dbbd27f606453744
felipe.silva <felipe.silva@ipm.com.br>
2022-07-11 16:46:13 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/bcrypt_adapter.spec.ts
+++ src/infra/criptography/bcrypt_adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt_adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hash_value'))
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   test('Deverá chamar o bcrypt com o valores corretos', async () => {
     const salt = 12
@@ -8,5 +14,12 @@ describe('Bcrypt Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Deverá retornar o hash caso o valor do password esteja correto', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash_value')
   })
 })
