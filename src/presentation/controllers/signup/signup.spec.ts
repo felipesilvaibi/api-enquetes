934a147e1b60c5d5a627016ffc9524b2e19748ea
felipe.silva <felipe.silva@ipm.com.br>
2022-07-11 08:50:52 -0300

    refactor: move all sinup protocols to a single file

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,7 +1,5 @@
-import { AccountModel } from '../../domain/models/account'
-import { AddAccount, AddAccountModel } from '../../domain/usecases/add-account'
-import { MissingParamError, InvalidParamError, ServerError } from '../errors'
-import { EmailValidator } from '../protocols'
+import { MissingParamError, InvalidParamError, ServerError } from '../../errors'
+import { EmailValidator, AccountModel, AddAccount, AddAccountModel } from './signup_protocols'
 import { SignUpController } from './signup'
 
 const makeEmailValidator = (): EmailValidator => {
