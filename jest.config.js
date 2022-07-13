0df0e67e1c4169dae80c6c7e29ceaa10a5cc0d6c
felipe.silva <felipe.silva@ipm.com.br>
2022-07-13 16:11:56 -0300

    chore: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src/'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/**/index.ts',
+    '!<rootDir>/**/env.ts',
+    '!<rootDir>/**/*_protocols.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   // preset: '@shelf/jest-mongodb',
