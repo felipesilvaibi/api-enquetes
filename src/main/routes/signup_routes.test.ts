50ee6e7e582cb0a70556932cda8536968c9972ba
felipe.silva <felipe.silva@ipm.com.br>
2022-07-13 14:30:14 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup_routes.test.ts
+++ src/main/routes/signup_routes.test.ts
@@ -1,7 +1,20 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo_helper'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    // await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  })
+
+  afterAll(async () => {
+    // await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    // const accountCollection = MongoHelper.getCollection('accounts')
+    // await accountCollection.deleteMany({})
+  })
   test('Deverá retornar uma account em caso de sucesso', async () => {
     await request(app)
       .post('/api/signup')
